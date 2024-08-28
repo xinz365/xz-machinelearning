83ca84ce9a372cad0a5c538b3572eb4a730ed4ea
xinz365 <108641741+xinz365@users.noreply.github.com>
2024-08-28 11:58:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learning model!')
+
+df = pd.read_csv('blt_sample_streamlit.csv')
+df
