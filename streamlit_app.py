ba9bc60fbf58ad93712bb6695517e2a95be65a2a
xinz365 <108641741+xinz365@users.noreply.github.com>
2024-08-28 12:54:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ st.info('This app builds a machine learning model!')
 
 with st.expander('BLT Data'):
   st.write('**Raw Data**')
-  df = pd.read_csv('https://github.com/xinz365/xz-machinelearning/blob/master/blt_sample_streamlit.csv')
+  df = pd.read_csv('blt_sample_steamlit.csv')
   df
