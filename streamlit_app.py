943a67b3be7b02395fe63b261391a8a36ff6370e
xinz365 <108641741+xinz365@users.noreply.github.com>
2024-09-04 03:03:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,4 @@ with st.sidebar:
   st.header('Input Features')
   dispenserpressure_ecro = st.number_input('Dispenser Pressure')
   st.write("The current Dispenser Pressure is ", dispenserpressure_ecro)
+  
