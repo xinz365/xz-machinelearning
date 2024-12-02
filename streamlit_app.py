1596e017e9a42e86c28c5bb6bdc6c926b97372d7
xinz365 <108641741+xinz365@users.noreply.github.com>
2024-12-03 03:59:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,51 +24,27 @@ with st.expander('Data Visualization'):
 
 with st.sidebar:
   st.header('Input Features')
-  dispenserpressure_ecro = st.number_input('Dispenser Pressure', min_value=0.0, max_value=5.0, value=None, placeholder="Type a number...")
-  st.write("The current Dispenser Pressure is ", dispenserpressure_ecro)
-  bondprocessbondztouchheight_ecro = st.number_input('Bond Z Touch Height', min_value=0.0, max_value=1000.0, value=None, placeholder="Type a number...")
-  st.write("The current Bond Z Touch Height is ", bondprocessbondztouchheight_ecro)
-  dvipostdispenseshapecheckdatainner = st.number_input('Post Dispenser Shape Check Inner')
-  st.write("The current Post Dispenser Shape Check Inner is ", dvipostdispenseshapecheckdatainner)
-  dvipostdispenseshapecheckdataouter = st.number_input('Post Dispenser Shape Check Outer')
-  st.write("The current Post Dispenser Shape Check Outer is ", dvipostdispenseshapecheckdataouter)
-  pbiepoxycoverage_left = st.number_input('Epoxy Coverage Left')
-  st.write("The current Epoxy Coverage Left is ", pbiepoxycoverage_left)
-  pbiepoxycoverage_top = st.number_input('Epoxy Coverage Top')
-  st.write("The current Epoxy Coverage Top is ", pbiepoxycoverage_top)
-  pbiepoxycoverage_right = st.number_input('Epoxy Coverage Right')
-  st.write("The current Epoxy Coverage Right is ", pbiepoxycoverage_right)
-  pbiepoxycoverage_bottom = st.number_input('Epoxy Coverage Bottom')
-  st.write("The current Epoxy Coverage Bottom is ", pbiepoxycoverage_bottom)
-  bondtimenothighforcearray_ecro = st.slider('Bond Time Not High Force Array',50, 300, 150)
-  st.write("The current Bond Time Not High Force Array is ", bondtimenothighforcearray_ecro)
-  dispensepatternclassselection_ecro = st.selectbox('Dispense Pattern Class Selection',('20','40'))
-  st.write("The current Dispense Pattern Class Selection is ", dispensepatternclassselection_ecro)
-  dispenseprinttime_ecro = st.selectbox('Dispense Print Time','30')
-  st.write("The current Dispense Print Time is ", dispenseprinttime_ecro)
-  dispenseprocessspeed_ecro = st.selectbox('Dispense Process Speed',('0.7','0.75','0.8'))
-  st.write("The current Dispense Process Speed is ", dispenseprocessspeed_ecro)
-  dispensewaittime_ecro = st.slider('Dispense Wait Time',50, 200, 100)
-  st.write("The current Dispense Wait Time is ", dispensewaittime_ecro)
-  dispenseheighttostrip_ecro = st.selectbox('Dispense Height to Strip',('120','130','140'))
-  st.write("The current Dispense Height to Strip is ", dispenseheighttostrip_ecro)
-
-  data = {'dispenserpressure_ecro': dispenserpressure_ecro,
-          'bondtimenothighforcearray_ecro': bondtimenothighforcearray_ecro,
-          'dispensepatternclassselection_ecro': dispensepatternclassselection_ecro,
-          'dispenseprinttime_ecro': dispenseprinttime_ecro,
-          'dispenseprocessspeed_ecro': dispenseprocessspeed_ecro,
-          'dispensewaittime_ecro': dispensewaittime_ecro,
-          'dispenseheighttostrip_ecro': dispenseheighttostrip_ecro,
-          'dvipostdispenseshapecheckdatainner': dvipostdispenseshapecheckdatainner,
-          'dvipostdispenseshapecheckdataouter': dvipostdispenseshapecheckdataouter,
-          'pbiepoxycoverage_left': pbiepoxycoverage_left,
-          'pbiepoxycoverage_top': pbiepoxycoverage_top,
-          'pbiepoxycoverage_right': pbiepoxycoverage_right,
-          'pbiepoxycoverage_bottom': pbiepoxycoverage_bottom,
-          'bondprocessbondztouchheight_ecro': bondprocessbondztouchheight_ecro}
+  equipment_id = st.selectbox('EquipmentID', ('ES-161','ES-162'))
+  st.write("Equipment_ID is ", equipment_id)
+  date = st.selectbox('Date', ('2024-11-26','2024-11-27','2024-11-28','2024-11-29','2024-11-30'))
+  st.write("Date is ", date)
+  lot_id = st.selectbox('Lot_ID', ('SUPD35DNV000','SUPD35DNW000','SUPD35DNZ000','SUPD35DQR000','SUPD35EM0000'))
+  st.write("Lot_ID is ", lot_id)
+  strip_id = st.selectbox(Strip_ID, ('P131584725_0014','P131584725_0015','P131584725_0016','P131584725_0017','P131584725_0018'))
+  st.write("Strip_ID is ", strip_id)
+  die_position_x = st.selectbox('X', ('1','2','3','4','5'))
+  st.write("Die Position X is ", die_position_x)
+  die_position_y = st.selectbox('Y', ('1','2','3','4','5'))
+  st.write("Die Position Y is ", die_position_y)
+  
+  data = {'equipment_id': equipment_id,
+          'date': date,
+          'lot_id': lot_id,
+          'strip_id': strip_id,
+          'die_position_x': die_position_x,
+          'die_position_y': die_position_y}
   input_df = pd.DataFrame(data, index=[0])
-  input_sample = pd.concat([input_df, X], axis=0)
+  input_sample = pd.concat([input_df, X], axis=1)
 
 st.info('**Please Confirm Your Input**')
 input_df
